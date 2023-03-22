54e487f7f06162d8b5ade21529efed4742acec0d
Tess Phillips <tess.scarlet@gmail.com>
2023-03-22 18:23:46 +0000

    keyboard press added for accessibility

--- myWebjs.js
+++ myWebjs.js
@@ -1,5 +1,23 @@
+const Ids = ["T","E","S","S2","P","H","I","L","L2","I2","P2","S3"]
+const paraId2 = ["Tpara","Epara","Spara","S2para","Ppara","Hpara","Ipara","Lpara","L2para","I2para","P2para","S3para"]
+var j=0;
+
+function closeAll(){
+    paraId2.forEach(element => {
+        document.getElementById(element).style.display ="none";
+    });
+}
+
+function toggleKey(){
+    closeAll()
+    document.getElementById(paraId2[j]).style.display ="block";
+    j+=1;
+    if (j==12){j=0}
+}
+ 
+document.addEventListener("keydown",()=>toggleKey());
+
 function toggle(paraIdClicked){
-    const Ids = ["T","E","S","S2","P","H","I","L","L2","I2","P2","S3"]
     if (document.getElementById(paraIdClicked).style.display =="block"){
         document.getElementById(paraIdClicked).style.display ="none"
     }
